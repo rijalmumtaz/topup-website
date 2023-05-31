b13508b2548064c62ab69c160313c08db77de063
rijalmumtaz <aularrijalalmumtaz@gmail.com>
2023-05-31 20:18:13 +0700

    Refactor Menu Sidebar Overview

---
+++ components/organisms/SideBar/MenuItem.tsx
@@ -0,0 +1,47 @@
+import Image from "next/image";
+import Link from "next/link";
+import cx from "classnames";
+
+interface MenuItemProps {
+  title: string;
+  href: string;
+  img:
+    | "overview"
+    | "transaction"
+    | "messages"
+    | "card"
+    | "rewards"
+    | "setting"
+    | "log-out";
+  active: boolean;
+}
+
+export default function MenuItem(props: Partial<MenuItemProps>) {
+  const { title, href = "/member", img, active } = props;
+
+  const classMenuItem = cx({
+    item: true,
+    "mb-30": true,
+    active: active,
+  });
+
+  return (
+    <div className={classMenuItem}>
+      <Image
+        className="icon me-3"
+        src={`/icon/ic-menu-${img}.svg`}
+        width={25}
+        height={25}
+        alt="overview"
+      />
+      <p className="item-title m-0">
+        <Link
+          legacyBehavior
+          href={href}
+        >
+          <a className="text-lg text-decoration-none">{title}</a>
+        </Link>
+      </p>
+    </div>
+  );
+}
---
+++ components/organisms/SideBar/index.tsx
@@ -0,0 +1,52 @@
+import Footer from "./Footer";
+import MenuItem from "./MenuItem";
+import Profile from "./Profile";
+
+export default function SideBar() {
+  return (
+    <section className="sidebar">
+      <div className="content pt-50 pb-30 ps-30">
+        <Profile />
+        <div className="menus">
+          <MenuItem
+            href="/member"
+            img="overview"
+            title="Overview"
+            active
+          />
+          <MenuItem
+            href="/member"
+            img="transaction"
+            title="Transactions"
+          />
+          <MenuItem
+            href="/member"
+            img="messages"
+            title="Messages"
+          />
+          <MenuItem
+            href="/member"
+            img="card"
+            title="Card"
+          />
+          <MenuItem
+            href="/member"
+            img="rewards"
+            title="Rewards"
+          />
+          <MenuItem
+            href="/member"
+            img="setting"
+            title="Settings"
+          />
+          <MenuItem
+            href="/member"
+            img="log-out"
+            title="Log Out"
+          />
+        </div>
+        <Footer />
+      </div>
+    </section>
+  );
+}
