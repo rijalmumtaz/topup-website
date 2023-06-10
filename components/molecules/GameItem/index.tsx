bce10e46835037018dc0a06f1f0fce361df1d51f
rijalmumtaz <aularrijalalmumtaz@gmail.com>
2023-06-10 16:07:06 +0700

    Update index.tsx

--- components/molecules/GameItem/index.tsx
+++ components/molecules/GameItem/index.tsx
@@ -20,6 +20,7 @@ export default function GameItem(props: GameItemProps) {
         legacyBehavior
         href="/detail"
       >
+        {/* commit */}
         <a>
           <div className="blur-sharp">
             <Image
