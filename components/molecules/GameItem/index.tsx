f4d9d5ebcf5adafa57cb093fa9926ff2a724561f
rijalmumtaz <aularrijalalmumtaz@gmail.com>
2023-06-13 19:50:06 +0700

    Update index.tsx

--- components/molecules/GameItem/index.tsx
+++ components/molecules/GameItem/index.tsx
@@ -20,6 +20,7 @@ export default function GameItem(props: GameItemProps) {
         legacyBehavior
         href="/detail"
       >
+        {/* kenapa ya */}
         <a>
           <div className="blur-sharp">
             <Image
