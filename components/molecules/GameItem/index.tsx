015fc046465004b0d25cca86ea9fb8ea32eb2685
rijalmumtaz <aularrijalalmumtaz@gmail.com>
2023-06-18 23:58:54 +0700

    Update index.tsx

--- components/molecules/GameItem/index.tsx
+++ components/molecules/GameItem/index.tsx
@@ -21,7 +21,6 @@ export default function GameItem(props: GameItemProps) {
         href="/detail"
       >
         <a>
-        {/* salkdalskd */}
           <div className="blur-sharp">
             <Image
               className="thumbnail"
